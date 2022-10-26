ef42dd7ddc6db40f6b4bd63ea3b61272e5088022
Mauro Erni <Mauro_Erni@sluz.ch>
2022-10-26 13:16:08 +0200

    Unittest Aufgabe 1-3

--- hello-csharp/Calculator.Tests/CalculatorTest.cs
+++ hello-csharp/Calculator.Tests/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace Calculator.Tests
@@ -8,6 +9,30 @@ namespace Calculator.Tests
         public void FivePlusThreeIsEight()
         {
             Assert.Equal(8, Calculator.Add(3, 5));
+        }
+
+        [Fact]
+        public void TestAddThreeToTwoIsFive()
+        {
+            Assert.Equal(5, Calculator.Add(3, 2));
+        }
+
+        [Fact]
+        public void TestSubtractFourFromSixIsTwo()
+        {
+            Assert.Equal(2, Calculator.Substract(6, 4));
+        }
+
+        [Fact]
+        public void TestMultiplyFourAndTwoIsEight()
+        {
+            Assert.Equal(8, Calculator.Multiply(4, 2));
+        }
+
+        [Fact]
+        public void TestDivideWithZero()
+        {
+            Assert.Throws<ArgumentException>(() => Assert.Equal(0, Calculator.divide(6, 0));
         }
     }
 }
--- hello-csharp/Calculator/Calculator.cs
+++ hello-csharp/Calculator/Calculator.cs
@@ -1,10 +1,31 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
     public class Calculator
     {
         public static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int divide(int a, int b)
+        {
+            if(b == 0)
+            {
+                throw new ArgumentException();
+            }
+            return a / b;
         }
     }
 }
